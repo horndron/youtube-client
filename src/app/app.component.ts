9fce920d3cfc17ade651c176869cbe249121e65e
horndron <horndron@yandex.ru>
2022-03-22 18:21:11 +0300

    feat: add eslint and prettier

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass'],
+})
+export default class AppComponent {
+  title = 'youtube-client';
+}
